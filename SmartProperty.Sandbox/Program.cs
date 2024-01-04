e4d8df6ae21737e8a0028cf7469968df90e49a94
Martin Boje Carpentier <mbc@kamstrup.com>
2024-01-04 09:58:04 +0100

    Updated program

--- SmartProperty.Sandbox/Program.cs
+++ SmartProperty.Sandbox/Program.cs
@@ -8,17 +8,20 @@ using SmartProperty;
 
 var generator = SmartPropertyFactory.Create();
 
-string field = "[bogus.parse('{{Name.FirstName}}')]";
+string field = "[Bogus.ParseLocaleWithSeed('{{name.firstname(Male)}} {{name.lastname}}', 'en_US', 1241241)]";
 string generatedProperty = "";
 if (generator.IsValidSmartProperty(field, out var errorMessage))
 {
-    generatedProperty = generator.Generate(field);
+    for (int i = 0; i < 100; i++)
+    {
+        generatedProperty = generator.Generate(field);
+        Console.WriteLine(generatedProperty);
+    }
 }
 else
 {
     Console.WriteLine(errorMessage);
 }
 
-Console.WriteLine(generatedProperty);
 
 Console.ReadKey();
